095eec4b0a6b13182dba4260c476ffd9b1541d87
Tpleme <tpleme@hotmail.com>
2023-08-26 16:00:01 +0100

    started working on image picker and portrait upload

--- client/Components/Modals/ProfileModals/ChangePassword.js
+++ client/Components/Modals/ProfileModals/ChangePassword.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import Modal from 'react-native-modal'
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import CustomButton from '../../Buttons/CustomButton';
 import { useForm, Controller } from 'react-hook-form'
 import { PasswordTextInput } from '../../Inputs/TextInputs'
 import { changePassword } from '../../../API/requests';
+import backgroundImage from '../../../assets/backgrounds/banner.jpg'
 
 function ChangePassword(props) {
     const [loading, setLoading] = useState(false)
@@ -35,78 +36,81 @@ function ChangePassword(props) {
                 props.close();
             }}
         >
-            <View style={styles.modalView}>
-                <Text style={styles.title}>Use the following form to change your password</Text>
-                <View style={styles.form}>
-                    <Text style={{ color: 'white', marginTop: 10 }}>New Password</Text>
-                    <Controller
-                        control={control}
-                        name="newPassword"
-                        defaultValue=''
-                        rules={{
-                            required: 'New Password required',
-                            pattern: { value: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20})/, message: 'Password must have at least 8 characters and include, lower, upper case and numbers' },
-                        }}
-                        render={({ field: { onChange, value } }) => (
-                            <PasswordTextInput
-                                value={value}
-                                type="password"
-                                placeholder=''
-                                onChange={onChange}
-                                error={Boolean(errors.newPassword)}
-                                helperText={errors.newPassword?.message}
-                            />
-                        )}
-                    />
+            <View style={{ flexDirection: 'column', height: '100%' }}>
+                <Image style={styles.image} source={backgroundImage} />
+                <View style={styles.modalView}>
+                    <Text style={styles.title}>Use the following form to change your password</Text>
+                    <View style={styles.form}>
+                        <Text style={{ color: 'white', marginTop: 10 }}>New Password</Text>
+                        <Controller
+                            control={control}
+                            name="newPassword"
+                            defaultValue=''
+                            rules={{
+                                required: 'New Password required',
+                                pattern: { value: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20})/, message: 'Password must have at least 8 characters and include, lower, upper case and numbers' },
+                            }}
+                            render={({ field: { onChange, value } }) => (
+                                <PasswordTextInput
+                                    value={value}
+                                    type="password"
+                                    placeholder=''
+                                    onChange={onChange}
+                                    error={Boolean(errors.newPassword)}
+                                    helperText={errors.newPassword?.message}
+                                />
+                            )}
+                        />
 
-                    <Text style={{ color: 'white', marginTop: 20 }}>Confirm New Password</Text>
-                    <Controller
-                        control={control}
-                        name="confirmPassword"
-                        defaultValue=''
-                        rules={{
-                            required: 'Confirm Password required',
-                            validate: pass => {
-                                if (watch('newPassword') !== pass) {
-                                    return 'Passwords do not match'
+                        <Text style={{ color: 'white', marginTop: 20 }}>Confirm New Password</Text>
+                        <Controller
+                            control={control}
+                            name="confirmPassword"
+                            defaultValue=''
+                            rules={{
+                                required: 'Confirm Password required',
+                                validate: pass => {
+                                    if (watch('newPassword') !== pass) {
+                                        return 'Passwords do not match'
+                                    }
                                 }
-                            }
-                        }}
-                        render={({ field: { onChange, value } }) => (
-                            <PasswordTextInput
-                                value={value}
-                                placeholder=''
-                                type="password"
-                                onChange={onChange}
-                                error={Boolean(errors.confirmPassword)}
-                                helperText={errors.confirmPassword?.message}
-                            />
-                        )}
-                    />
+                            }}
+                            render={({ field: { onChange, value } }) => (
+                                <PasswordTextInput
+                                    value={value}
+                                    placeholder=''
+                                    type="password"
+                                    onChange={onChange}
+                                    error={Boolean(errors.confirmPassword)}
+                                    helperText={errors.confirmPassword?.message}
+                                />
+                            )}
+                        />
 
-                    <Text style={{ color: 'white', marginTop: 20 }}>Your current Password</Text>
-                    <Controller
-                        control={control}
-                        name="authPassword"
-                        defaultValue=''
-                        rules={{
-                            required: 'Current Password required',
-                        }}
-                        render={({ field: { onChange, value } }) => (
-                            <PasswordTextInput
-                                value={value}
-                                type="password"
-                                placeholder=''
-                                onChange={onChange}
-                                error={Boolean(errors.authPassword)}
-                                helperText={errors.authPassword?.message}
-                            />
-                        )}
-                    />
-                </View>
-                <View style={styles.actionsView}>
-                    <CustomButton loading={loading} label={loading ? 'Submitting...' : 'Submit'} onPress={handleSubmit(onSubmit)} />
-                    <CustomButton disabled={loading} color='darkgrey' label='Cancel' onPress={() => props.close()} />
+                        <Text style={{ color: 'white', marginTop: 20 }}>Your current Password</Text>
+                        <Controller
+                            control={control}
+                            name="authPassword"
+                            defaultValue=''
+                            rules={{
+                                required: 'Current Password required',
+                            }}
+                            render={({ field: { onChange, value } }) => (
+                                <PasswordTextInput
+                                    value={value}
+                                    type="password"
+                                    placeholder=''
+                                    onChange={onChange}
+                                    error={Boolean(errors.authPassword)}
+                                    helperText={errors.authPassword?.message}
+                                />
+                            )}
+                        />
+                    </View>
+                    <View style={styles.actionsView}>
+                        <CustomButton loading={loading} label={loading ? 'Submitting...' : 'Submit'} onPress={handleSubmit(onSubmit)} />
+                        <CustomButton disabled={loading} color='darkgrey' label='Cancel' onPress={() => props.close()} />
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -120,18 +124,17 @@ const styles = StyleSheet.create({
         margin: 0,
     },
     modalView: {
-        borderRadius: 10,
         width: '100%',
         backgroundColor: '#202020',
-        height: '100%',
-        padding: 20,
+        flex: 1,
+        paddingHorizontal: 20,
+        paddingBottom: 20,
         alignItems: 'center'
     },
     title: {
         color: 'white',
         fontSize: 20,
         textAlign: 'center',
-        marginTop: 20
     },
     form: {
         width: '100%',
@@ -142,4 +145,8 @@ const styles = StyleSheet.create({
         gap: 10,
         width: '100%',
     },
+    image: {
+        height: 200,
+        resizeMode: 'stretch',
+    }
 });
--- client/Components/Modals/ProfileModals/ProfileModal.js
+++ client/Components/Modals/ProfileModals/ProfileModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Modal from 'react-native-modal'
 import { StyleSheet, View, Text, Image } from 'react-native';
 import { useUserInfo } from '../../../Hooks/useUser';
@@ -9,13 +9,37 @@ import CustomButton from '../../Buttons/CustomButton';
 import ChangePassword from './ChangePassword';
 import EditProfile from './EditProfile';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-
+import backgroundImage from '../../../assets/backgrounds/banner.jpg'
+import * as ImagePicker from 'expo-image-picker';
 
 function ProfileModal(props) {
     const insets = useSafeAreaInsets();
     const { userInfo, setUserInfo } = useUserInfo()
     const [openChangePass, setOpenChangePass] = useState(false)
     const [openEditProfile, setOpenEditProfile] = useState(false)
+    const [protrait, setPortrait] = useState(null)
+
+    useEffect(() => {
+        if (props.open) {
+            console.log(userInfo)
+        }
+
+    }, [props])
+
+    const pickPortrait = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            quality: 1,
+            base64: true
+        });
+        if (!result.canceled) {
+            console.log(result);
+            setPortrait(result.assets[0].uri);
+        } else {
+            alert('You did not select any image.');
+        }
+    }
+
 
     return (
         <Modal
@@ -29,12 +53,13 @@ function ProfileModal(props) {
             }}
         >
             <View style={styles.modalView}>
+                <Image style={styles.topImage} source={backgroundImage} />
                 <View style={{ ...styles.backButton, paddingTop: insets.top + 10 }}>
                     <Ionicons name='arrow-back-outline' size={32} color='white' onPress={() => props.close()} />
                 </View>
                 <View style={styles.portraitWrapper}>
                     <Image alt='portrait' source={userInfo.picture ? `${SERVER_URL}/resources/images/app_users/${userInfo.picture}` : WomanPortrait} style={styles.portrait} />
-                    <CustomButton icon='pencil' label='Change Portrait' />
+                    <CustomButton icon='pencil' label='Change Portrait' onPress={pickPortrait} />
                     <CustomButton icon='delete' label='Remove Portrait' />
                 </View>
                 <View style={styles.userInfoView} >
@@ -73,7 +98,9 @@ const styles = StyleSheet.create({
         width: 200,
         height: 200,
         borderRadius: 50,
-        resizeMode: 'cover'
+        resizeMode: 'cover',
+        borderColor: '#202020',
+        borderWidth: 5
     },
     userInfoView: {
         marginTop: 50,
@@ -92,4 +119,9 @@ const styles = StyleSheet.create({
         width: '100%',
         zIndex: 500
     },
+    topImage: {
+        height: 200,
+        resizeMode: 'stretch',
+        position: 'absolute'
+    }
 });
