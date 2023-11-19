8f5c2de12f3efa3986928a053fbe303cd3062bc6
Tpleme <tpleme@hotmail.com>
2023-11-19 13:51:46 +0000

    added url to sequelize instance

--- server/database/sequelize_index.js
+++ server/database/sequelize_index.js
@@ -1,9 +1,13 @@
 const Sequelize = require('sequelize')
 require('dotenv').config();
 
-const database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+// const database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql'
+// })
+
+const database = new Sequelize(process.env.DB_URL, {
     dialect: 'mysql'
 })
 
